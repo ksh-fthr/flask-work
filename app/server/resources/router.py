eee6e47034dbe121d3f5ed10590c07a96601c289
Hayato Inoue <h_inoue@webfrontier.co.jp>
2019-06-25 12:25:32 +0900

    ZIPファイルの生成と返却を行うAPIサンプルを作った

--- app/server/resources/router.py
+++ app/server/resources/router.py
@@ -9,3 +9,4 @@ from server.resources.logout_refresh import UserLogoutRefresh
 from server.resources.token_refresh import TokenRefresh
 from server.resources.user_registration import UserRegistration
 from server.resources.csv import Csv
+from server.resources.zip import Zip
